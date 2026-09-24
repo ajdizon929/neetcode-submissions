d80841992ea761b60051e698338bbf8c666f4e16
ajdizon929 <adizon929@gmail.com>
2026-09-24 21:48:34 +1000

    Add: sql-sales-person - submission-0

---
+++ SQL/sql-sales-person/submission-0.sql
@@ -0,0 +1,10 @@
+-- Write your query below
+SELECT s.name
+FROM sales_person AS s
+WHERE s.sales_id NOT IN (
+    SELECT o.sales_id
+    FROM orders AS o
+    INNER JOIN company AS c
+        ON o.com_id = c.com_id
+    WHERE c.name = 'CRIMSON'
+);
